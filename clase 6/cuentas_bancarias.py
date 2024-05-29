4a87c703975adae40f3fc5c0a456145849441b1a
Stefania Silva <stefania.silva19@gmail.com>
2024-05-29 17:45:29 -0300

    primer bosquejo de todas las clases

--- clase 6/cuentas_bancarias.py
+++ clase 6/cuentas_bancarias.py
@@ -1,45 +1,58 @@
-#clase abstracta
+
 from abc import ABC, abstractmethod
+import datetime
 
 
 class CuentaBancaria (ABC): 
-    
-    def __init__(self, *kwargs):
-        self.nro_cuenta = kwargs["nro_cuenta"]
-        self.cbu= kwargs["cbu"]
-        self.alias= kwargs["alias"]
-        self.saldo= kwargs["saldo"]
+    def __init__(self, *args):
+        self.nro_cuenta = args["nro_cuenta"]
+        self.cbu= args["cbu"]
+        self.alias= args["alias"]
+        self.saldo= args["saldo"]
         self.movimientos =[]
     
     def consultar_saldo(self) -> float:
         return self.saldo; 
-       
+    
+    def registrarMovimientos(self, tipoMovimiento:str, monto:float): 
+        movimiento:tuple = ( datetime.now(), tipoMovimiento, monto, self.saldo)
+        self.movimientos.append(movimiento) 
+          
     def depositar(self, monto_a_depositar:float) -> bool: 
-        self.saldo += monto_a_depositar
-            
+        try: 
+            self.saldo += monto_a_depositar
+            self.registrarMovimientos("deposito", monto_a_depositar)
+            return True
+        except: 
+            return False 
+
     
     @abstractmethod
     def extraer(self, monto_a_extraer:float)-> bool:
         pass
+      
     
     @abstractmethod
     def transferir(self,monto_a_transferir:float) -> bool:
         pass
-    
+        
+       
 class CajaDeAhorro(CuentaBancaria):
     
-   def __init__(self,monto_extrac:float,monto_transfer:float,cant_extrac:int, cant_transfer: int, *kwargs):
-       super().__init__(*kwargs)
+   def __init__(self, nro_cuenta, alias, cbu, saldo ,monto_extrac:float,monto_transfer:float,cant_extrac:int, cant_transfer: int):
+       super().__init__(nro_cuenta, alias, cbu, saldo,)
        self.monto_limite_extracciones = monto_extrac #float
        self.monto_limite_transferencias = monto_transfer #float
        self.cant_extracciones_disponibles = cant_extrac#int
        self.cant_transferencias_disponibles = cant_transfer #int
        
    def extraer(self, monto_a_extraer: float) -> bool:
-        if (self.monto_limite_extracciones > monto_a_extraer < self.saldo and self.cant_extracciones_disponibles > 0):
-            self.saldo -= monto_a_extraer
+        if (self.monto_limite_extracciones > monto_a_extraer < self.saldo and self.cant_extracciones_disponibles > 0):    
             self.monto_limite_extracciones -= 1
+            self.saldo -= monto_a_extraer
+            self.registrarMovimientos("extraccion", monto_a_extraer)
             return True
+       
         else:
             return False   
           
@@ -47,46 +60,110 @@ class CajaDeAhorro(CuentaBancaria):
         if(self.monto_limite_transferencias > monto_a_transferir < self.saldo and self.cant_transferencias_disponibles > 0):
             self.saldo -= monto_a_transferir
             self.monto_limite_transferencias -= 1
+            self.registrarMovimientos("Transferencia", monto_a_transferir)
             return True
         else:
             return False
-    
-    
 
-
-'''
-La clase CajaDeAhorro debe contener:
-
-
-• Métodos de instancia:
-o extraer (monto_a_extraer de tipo float), que retorna un booleano indicando si la operación
-se realizó correctamente o no. La extracción se considerará como correcta si
-monto_a_extraer es mayor a cero, si monto_a_extraer no es superior al saldo, si
-monto_a_extraer no es superior al monto_limite_extracciones y si la
-cant_extracciones_disponibles es mayor a cero, en cuyo caso se deberá actualizar el saldo
-y cant_extracciones_disponibles; e invocar al método heredado para registrar el
-movimiento.
-o transferir (monto_a_transferir de tipo float, cuenta_destino de tipo CuentaBancaria), que
-retorna un booleano indicando si la operación se realizó correctamente o no. La
-transferencia se considerará como correcta si monto_a_transferir es mayor a cero, si
-monto_a_transferir no es superior al saldo, si monto_a_transferir no es superior al
-monto_limite_transferencias y si la cant_transferencias_disponibles es mayor a cero, en
-cuyo caso se deberá actualizar el saldo y cant_transferencias_disponibles de la cuenta
-origen y el saldo de la cuenta destino; e invocar al método heredado para registrar el
-movimiento.
-'''
 
 class CuentaCorriente(CuentaBancaria): 
-    pass
+    def __init__(self, nro_cuenta, alias, cbu, saldo, monto_maximo_descubierto:float):
+        super().__init__(nro_cuenta, alias, cbu, saldo)
+        self.monto_maximo_descubierto:float = monto_maximo_descubierto
+        
+    def extraer(self, monto_a_extraer: float) -> bool:
+        if (self.saldo> monto_a_extraer > 0 and self.monto_maximo_descubierto > monto_a_extraer):
+            self.saldo -= monto_a_extraer
+            self.registrarMovimientos("extraccion", monto_a_extraer)
+            return True
+        else:
+            return False
+        
+    def transferir(self, monto_a_transferir: float, cuenta_destino:str) -> bool:
+        if (monto_a_transferir > 0 and monto_a_transferir < self.saldo):
+            self.saldo -= monto_a_transferir
+            self.registrarMovimientos("Transferencia", monto_a_transferir)
+            return True
+        else: 
+            return False
 
 class Cliente(): 
     #atributo lista de CuentasBancarias
-    def __init__(self):
-        self.list_cuentas = []
+    def __init__(self, *args):
+        self.razon_social:str = args['razon_social']
+        self.cuit:str = args['cuit']
+        self.tipo_persona:str = args['tipo_persona']
+        self.domicilio:str = args['domicilio']
+        self.cuentas_bancarias:CuentaBancaria = []
+    
+    def crear_nueva_cuenta_bancaria(self, *args) -> bool: 
+        if args['tipoCuenta'] == 'Ahorro':
+            try: 
+                self.cuentas_bancarias.append(CajaDeAhorro(args))
+                return True
+            except:
+                return False
+        if args['tipoCuenta'] == 'Corriente':
+            try: 
+                self.cuentas_bancarias.append(CuentaCorriente(args))
+                return True
+            except:
+                return False
+    
+        return False
+        
 
 class Banco():
-    def __init__(self) -> None:
-        self.list_clientes = []
+    def __init__(self, *args) -> None:
+        self.nombre:str = args['nombre']
+        self.domicilio:str = args['domicilio']
+        self.clientes:Cliente = []
+        
+    def crear_nuevo_cliente(self, razon_social, cuit, tipo_persona, domicilio)-> bool:
+        try:
+            nuevo_cliente = Cliente({ 'razon_social': razon_social,},{'cuit': cuit}, {'tipo_persona':tipo_persona}, {'domicilio': domicilio})
+            self.clientes.append(nuevo_cliente)
+            return True
+        except: 
+            return False            
     
-   
-   
+
+def main(): 
+    santander = Banco({'nombre': 'Santander'}, {'domicilio': 'Calle 123'})
+
+    santander.crear_nuevo_cliente({'razon social' :'Juan Perez'}, {'cuit': '20-12345678-9'},{'tipo_persona':'fisica'}, { 'domicilio': 'Direcion otra 456'})
+    
+    santander.crear_nuevo_cliente( {'razon social' :'Empresas topitos'}, {'cuit': '30-12345678-9'},
+    {'tipo_persona':'juridica'}, { 'domicilio': 'Direcion empresa 123'}
+    )
+    santander.crear_nuevo_cliente(    {'razon social' :'Ana Gomez'}, {'cuit': '27-32658942-1'},
+    {'tipo_persona':'fisica'}, { 'domicilio': 'Another 4654'}
+    )
+    
+    
+    
+if __name__ == '__main__':
+    main()
+
+
+
+'''
+Para validar el modelo de gestión bancaria implementado, incluya una función main() con las
+instrucciones necesarias para crear un objeto Banco, tres instancias de Clientes (como mínimo)
+cada uno de ellos con dos objetos CuentaBancaria (como mínimo), uno de tipo CajaDeAhorro y
+otro de tipo CuentaCorriente.
+
+
+Luego simule varias operaciones de depósito, extracción y transferencias entre las cuentas.
+Finalmente, muestre por pantalla los datos de los clientes del banco, con los saldos de sus cuentas
+y el registro de los movimientos de las mismas.
+
+
+Nota:
+Todos los atributos de instancia declarados en las clases deberán ser privados y accesibles
+mediante propiedades públicas de tipo getters y setters (usando el decorador @property).
+Para trabajar con fechas debe importar el módulo datetime.
+
+
+
+'''
